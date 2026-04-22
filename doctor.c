2000c41ba3612329286a34e3458f76e7dcc14d77
dafna2307 <dafna2510001@ssn.edu.in>
2026-04-22 14:37:11 +0530

    Update doctor.c

--- doctor.c
+++ doctor.c
@@ -1,10 +1,32 @@
-int main(int argc, char *argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Record {
+    int id;
+    char name[50];
+    char diagnosis[100];
+    char prescription[100];
+};
+
+int main() {
     struct Record r;
 
-    r.id = atoi(argv[1]);
-    strcpy(r.name, argv[2]);
-    strcpy(r.diagnosis, argv[3]);
-    strcpy(r.prescription, argv[4]);
+    printf("Enter Patient ID: ");
+    scanf("%d", &r.id);
+    getchar(); // consume leftover newline
+
+    printf("Enter Name: ");
+    fgets(r.name, sizeof(r.name), stdin);
+    r.name[strcspn(r.name, "\n")] = '\0';
+
+    printf("Enter Diagnosis: ");
+    fgets(r.diagnosis, sizeof(r.diagnosis), stdin);
+    r.diagnosis[strcspn(r.diagnosis, "\n")] = '\0';
+
+    printf("Enter Prescription: ");
+    fgets(r.prescription, sizeof(r.prescription), stdin);
+    r.prescription[strcspn(r.prescription, "\n")] = '\0';
 
     FILE *fp = fopen("records.dat", "ab");
 
